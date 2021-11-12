5a199659b99c08bbb0655e9cacf9301134e878a0
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-12 20:01:06 +0300

    Initial Commit

---
+++ YarikCard/YarikCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  YarikCardApp.swift
+//  YarikCard
+//
+//  Created by Yaroslav Monastyrev on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct YarikCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
