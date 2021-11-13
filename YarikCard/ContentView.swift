a69aac7d644d8b5e4164cb22290d5452dcfdcf3f
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-13 17:28:16 +0300

    Added Photo in circle and phone number

--- YarikCard/ContentView.swift
+++ YarikCard/ContentView.swift
@@ -11,11 +11,29 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(red: 0.15, green: 0.68, blue: 0.38, opacity: 1.00).edgesIgnoringSafeArea(.all)
-            Text("Yaroslav Monastyrev")
-                .font(.title)
-                .bold()
-                .foregroundColor(.white)
-            .padding()
+            VStack {
+                Image("yarik")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5))
+                Text("Yaroslav Monastyrev")
+                    .font(Font.custom("WorkSans-VariableFont_wght", size: 33))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 30))
+                Divider()
+                RoundedRectangle(cornerRadius: 30)
+                    .fill(Color.white)
+                    .frame(width: 300, height: 40)
+                    .overlay(Text("+7 111 111 11 11"))
+                
+            }
         }
     }
     
