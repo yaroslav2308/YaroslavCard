ec66f3e2b5de63d94f494c278a3a5ad249b28bab
yaroslav2308 <yaroslav.monastyrev@gmail.com>
2021-11-13 16:51:37 +0300

    First commit

--- YarikCard/ContentView.swift
+++ YarikCard/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(red: 0.15, green: 0.68, blue: 0.38, opacity: 1.00).edgesIgnoringSafeArea(.all)
+            Text("Yaroslav Monastyrev")
+                .font(.title)
+                .bold()
+                .foregroundColor(.white)
             .padding()
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice("iPhone 13")
     }
 }
